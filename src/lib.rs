4a99f4f736fd56ff8f1642faf8c3b2cc8ddaeb29
Drew Crawford <drew@sealedabstract.com>
2021-08-30 17:24:30 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+mod cgcolor;
+
+use objr::bindings::*;
+
+pub use cgcolor::CGColorRef;
+
+pub type CGFloat = f64;
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct CGPoint {
+    pub x: CGFloat,
+    pub y: CGFloat
+}
+unsafe impl Primitive for CGPoint {}
+unsafe impl Arguable for CGPoint {}
+#[repr(C)]
+#[derive(Debug)]
+pub struct CGSize {
+    pub width: CGFloat,
+    pub height: CGFloat
+}
+unsafe impl Primitive for CGSize {}
+unsafe impl Arguable for CGSize {}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct CGRect {
+    pub origin: CGPoint,
+    pub size: CGSize
+}
+unsafe impl Primitive for CGRect {}
+unsafe impl Arguable for CGRect{}
+impl CGRect {
+    pub fn make(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat) -> CGRect {
+        CGRect {
+            origin: CGPoint { x, y },
+            size: CGSize { width, height}
+        }
+    }
+}
